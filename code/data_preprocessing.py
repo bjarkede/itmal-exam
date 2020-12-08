dbf5fac4d5694e29fd79ef8e6cd30671e04f8b2f
Jonas-au18 <Jonas.sdu@gmail.com>
2020-12-08 11:18:09 +0100

    forgot to push

--- code/data_preprocessing.py
+++ code/data_preprocessing.py
@@ -3,8 +3,10 @@ from os import path
 import numpy as np
 import pickle
 import itertools
+from time import time
 
 import librosa
+import librosa.display
 import pandas as pd
 
 from scipy.stats import kurtosis
@@ -13,7 +15,7 @@ from scipy.stats import skew
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-local_directory = "E:\\bjark\\Desktop\\dataanalyse\\Data\\genres_original\\"
+local_directory = "C:\\Users\\jonas\\Desktop\\itmal-exame\\itmal-exam\\code\\"
 fileName = "features.dat"
 
 # Gets various statistics for each feature in the desc
@@ -26,16 +28,16 @@ def get_statistics(desc):
             result['{}_max'.format(k)] = []
             result['{}_min'.format(k)] = []
             result['{}_mean'.format(k)] = []
-            result['{}_std'.format(k)] = []
-            result['{}_kurtosis'.format(k)] = []
-            result['{}_skew'.format(k)] = []
+            #result['{}_std'.format(k)] = []
+            #result['{}_kurtosis'.format(k)] = []
+            #result['{}_skew'.format(k)] = []
             for v in values:
                 result['{}_max'.format(k)].append(np.max(v))
                 result['{}_min'.format(k)].append(np.min(v))
                 result['{}_mean'.format(k)].append(np.mean(v))
-                result['{}_std'.format(k)].append(np.std(v))
-                result['{}_kurtosis'.format(k)].append(kurtosis(v))
-                result['{}_skew'.format(k)].append(skew(v))
+                #result['{}_std'.format(k)].append(np.std(v))
+                #result['{}_kurtosis'.format(k)].append(kurtosis(v))
+                #result['{}_skew'.format(k)].append(skew(v))
     return result
 
 # For feature extraction we use Mel Frequency Cepstral Coefficients (MFCC)
@@ -43,8 +45,19 @@ def get_statistics(desc):
 def build_gtzan(path, f):
 
     # Features to build in the final dictionary
-    features = {'classlabel': [], 'mfcc_mean_matrix': [], 'variance': []}
+    features = {'classlabel': [], 'chroma_stft_mean':[],'chroma_stft_var':[],'rms_mean':[],
+           'rms_var':[],'bandwidth_mean':[],'bandwidth_var':[],'centroid_mean':[],
+           'centroid_var':[],'zero_crossing_mean':[],'zero_crossing_var':[],
+           'harmony_mean':[],'harmony_var':[],'rolloff_mean':[],'rolloff_var':[],
+           'perceptrual_mean':[],'perceptrual_var':[],'tempo':[]}
 
+    for i in range(20):
+        feat1 = f"mfcc{i+1}_mean"
+        feat2 = f"mfcc{i+1}_var"
+        features[feat1] = []
+        features[feat2] = []
+        
+    start = time()
     print("\n")
     folderIndex = 0    
     """Load GTZAN data from `path`"""
@@ -58,20 +71,100 @@ def build_gtzan(path, f):
         print("[",folderIndex,"/",10,"] Folders processed...")
         
         for file in os.listdir(path+folder):
-            y, sr = librosa.load(path+folder+"/"+file)
+            fileindex = 0
+            for i in range(10):
+                dur = 3.0 * fileindex
+                y, sr = librosa.load(path+folder+"/"+file,offset=dur,duration=3.0)
+                fileindex += 1
+                #Removing silence
+                y_trimmed,_ = librosa.effects.trim(y)
+                
+                #zero crossing
+                zeroX = librosa.feature.zero_crossing_rate(y_trimmed,pad=False)
+                zeroX_mean = zeroX.mean()
+                zeroX_var = zeroX.var()
+                
+                
+                #harmony & perceptrual
+                harm, perc = librosa.effects.hpss(y_trimmed)
+                harm_mean = harm.mean()
+                harm_var = harm.var()
+                perc_mean = perc.mean()
+                perc_var = perc.var()
+                
+                #Tempo
+                tempo,_ = librosa.beat.beat_track(y=y,sr=sr)
+                
+                #Centroid
+                centroid = librosa.feature.spectral_centroid(y=y_trimmed,sr=sr)[0]
+                centroid_mean = centroid.mean()
+                centroid_var = centroid.var()
+                
+                #Rolloff
+                rolloff = librosa.feature.spectral_rolloff(y=y_trimmed,sr=sr)[0]
+                rolloff_mean = rolloff.mean()
+                rolloff_var = rolloff.var()
+                
+                #Chroma frequencies
+                chroma = librosa.feature.chroma_stft(y=y_trimmed,sr=sr)
+                chroma_mean = chroma.mean()
+                chroma_var = chroma.var()
+            
+                #RMS
+                rms = librosa.feature.rms(y)
+                rms_mean = rms.mean()
+                rms_var = rms.var()
+            
+                #spectral bandwidth
+                bandwidth = librosa.feature.spectral_bandwidth(y,sr)
+                bandwidth_mean = bandwidth.mean()
+                bandwidth_var = bandwidth.var()
+            
+
+            
             # MFCC treatment
-            mfcc  = librosa.feature.mfcc(y, sr, n_mfcc=13)
-            variance = mfcc.var(0)
-            mean_matrix = mfcc.mean(0)
-            features['classlabel'].append(str(file).split('.', 1)[0])
-            features['mfcc_mean_matrix'].append(mean_matrix)
-            features['variance'].append(variance)
+                for i in range(20):
+                    mfcc  = librosa.feature.mfcc(y, sr, n_mfcc=i+1)
+                    mfcc_mean = mfcc.mean()
+                    mfcc_var = mfcc.var()
+                    feat1 = f"mfcc{i+1}_mean"
+                    feat2 = f"mfcc{i+1}_var"
+                    features[feat1].append(np.mean(mfcc_mean))
+                    features[feat2].append(np.mean(mfcc_var))
+            
 
-    dict_features = get_statistics(features)
-    dict_features['classlabel'] = features['classlabel']
-    df = pd.DataFrame(dict_features)
+                
+                
+                #adding features
+                features['classlabel'].append(str(file).split('.', 1)[0])
+                features['zero_crossing_mean'].append(zeroX_mean)
+                features['zero_crossing_var'].append(zeroX_var)
+                features['harmony_mean'].append(harm_mean)
+                features['harmony_var'].append(harm_var)
+                features['perceptrual_mean'].append(perc_mean)
+                features['perceptrual_var'].append(perc_var)
+                features['tempo'].append(tempo)
+                features['centroid_mean'].append(centroid_mean)
+                features['centroid_var'].append(centroid_var)
+                features['rolloff_mean'].append(rolloff_mean)
+                features['rolloff_var'].append(rolloff_mean)
+                features['chroma_stft_mean'].append(chroma_mean)
+                features['chroma_stft_var'].append(chroma_var)
+                features['rms_mean'].append(rms_mean)
+                features['rms_var'].append(rms_var)
+                features['bandwidth_mean'].append(bandwidth_mean)
+                features['bandwidth_var'].append(bandwidth_var)
+
+
+
+    #dict_features = get_statistics(features)
+    #dict_features['classlabel'] = features['classlabel']
+    #df = pd.DataFrame(dict_features)
+    df = pd.DataFrame(features)
     pickle.dump(df, f)
-
+    t = (time() - start) / 60
+    print((t))
+        
 # Used for compression features to a binary file.            
 def build_binaryfile(pathName):
     f = open(fileName, 'wb')
@@ -106,3 +199,89 @@ def partion_dataset(df):
     X_train, X_test, y_train, y_test =\
         train_test_split(X, y, test_size=0.3, random_state=0, stratify=y)
     return (stdsc.fit_transform(X_train), stdsc.transform(X_test), y_train, y_test)
+
+def get_features(pathtofile):
+    y, sr = librosa.load(pathtofile,duration=30)
+
+    #Removing silence
+    y_trimmed,_ = librosa.effects.trim(y)
+                
+    #zero crossing
+    zeroX = librosa.feature.zero_crossing_rate(y_trimmed,pad=False)
+    zeroX_mean = zeroX.mean()
+    zeroX_var = zeroX.var()
+                
+                
+    #harmony & perceptrual
+    harm, perc = librosa.effects.hpss(y_trimmed)
+    harm_mean = harm.mean()
+    harm_var = harm.var()
+    perc_mean = perc.mean()
+    perc_var = perc.var()
+                
+    #Tempo
+    tempo,_ = librosa.beat.beat_track(y=y,sr=sr)
+               
+    #Centroid
+    centroid = librosa.feature.spectral_centroid(y=y_trimmed,sr=sr)[0]
+    centroid_mean = centroid.mean()
+    centroid_var = centroid.var()
+                
+    #Rolloff
+    rolloff = librosa.feature.spectral_rolloff(y=y_trimmed,sr=sr)[0]
+    rolloff_mean = rolloff.mean()
+    rolloff_var = rolloff.var()
+                
+    #Chroma frequencies
+    chroma = librosa.feature.chroma_stft(y=y_trimmed,sr=sr)
+    chroma_mean = chroma.mean()
+    chroma_var = chroma.var()
+            
+    #RMS
+    rms = librosa.feature.rms(y)
+    rms_mean = rms.mean()
+    rms_var = rms.var()
+            
+    #spectral bandwidth
+    bandwidth = librosa.feature.spectral_bandwidth(y,sr)
+    bandwidth_mean = bandwidth.mean()
+    bandwidth_var = bandwidth.var()
+            
+
+            
+    # MFCC treatment
+    for i in range(20):
+        mfcc  = librosa.feature.mfcc(y, sr, n_mfcc=i+1)
+        mfcc_mean = mfcc.mean()
+        mfcc_var = mfcc.var()
+        feat1 = f"mfcc{i+1}_mean"
+        feat2 = f"mfcc{i+1}_var"
+        features[feat1].append(np.mean(mfcc_mean))
+        features[feat2].append(np.mean(mfcc_var))
+            
+
+                
+                
+        #adding features
+    features['classlabel'].append("")
+    features['zero_crossing_mean'].append(zeroX_mean)
+    features['zero_crossing_var'].append(zeroX_var)
+    features['harmony_mean'].append(harm_mean)
+    features['harmony_var'].append(harm_var)
+    features['perceptrual_mean'].append(perc_mean)
+    features['perceptrual_var'].append(perc_var)
+    features['tempo'].append(tempo)
+    features['centroid_mean'].append(centroid_mean)
+    features['centroid_var'].append(centroid_var)
+    features['rolloff_mean'].append(rolloff_mean)
+    features['rolloff_var'].append(rolloff_mean)
+    features['chroma_stft_mean'].append(chroma_mean)
+    features['chroma_stft_var'].append(chroma_var)
+    features['rms_mean'].append(rms_mean)
+    features['rms_var'].append(rms_var)
+    features['bandwidth_mean'].append(bandwidth_mean)
+    features['bandwidth_var'].append(bandwidth_var)
+        
+        
+    df = pd.DataFrame(features)
+    return df
